bf35f20b191a1c80cb33d2995433bea52a92f721
jeremy reiner <reiner_jeremy@yahoo.com>
2022-02-18 16:55:09 -0600

    restructure to portfolio repo

---
+++ base/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('posts/', views.posts, name="posts"),
+    path('post/<int:pk>', views.post, name="post"),
+    path('post/create/', views.postCreate, name="post_create"),
+    path('posts/search/', views.search_posts, name="search_posts"),
+    path('post/update/<int:pk>', views.postUpdate, name="post_update"),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+]
---
+++ base/views.py
@@ -0,0 +1,121 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post, Tag
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.db.models import Q
+# Create your views here.
+def home(request):
+    posts = Post.objects.filter(active=True, featured=True)
+    tags = Tag.objects.all()
+    context = {
+        'posts': posts.order_by,
+        'tags': tags
+        }
+    return render(request, "base/index.html", context)
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+
+    context = {
+        'post': post
+    }
+    return render(request, "base/post.html", context)
+
+
+def posts(request):
+    posts = Post.objects.filter(active=True).order_by('-created')
+    page = request.GET.get('page')
+    paginator = Paginator(posts, 3)
+
+    try: 
+        posts= paginator.page(page)
+    except PageNotAnInteger:
+        posts= paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'base/posts.html', context)
+
+
+@login_required(login_url='home')
+def postCreate(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'forms/post_form.html', context)
+
+
+@login_required(login_url='home')
+
+def postUpdate(request, pk):
+    post= Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form =PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'forms/post_form.html', context)
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url= '/'
+
+    def test_func(self):
+        post= self.get_object()
+        if self.request.user.is_authenticated:
+            return True
+        return False
+
+
+def search_posts(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        posts = Post.objects.filter(
+            Q(headline__icontains = searched) |
+            Q(sub_headline__icontains = searched) |
+            Q(body__icontains = searched)
+        )
+        page = request.GET.get('page')
+        paginator = Paginator(posts, 3)
+
+        try: 
+            posts= paginator.page(page)
+        except PageNotAnInteger:
+            posts= paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+
+        context = {
+            'searched': searched,
+            'posts':posts
+        }
+
+        return render(request, 'base/search_posts.html', context)
+    else:
+        return render(request, 'base/search_posts.html')
+
+def about(request):
+    return  render(request, 'base/my_story.html')
